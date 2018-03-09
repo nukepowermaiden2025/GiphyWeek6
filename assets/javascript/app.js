34ec924c4605bc9f3adcf2beb461cfcdbac4b4f3
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-08 18:14:51 -0700

    psuedo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//The essence of this homework is
+//Create an array to add items
+//Link those items to the page with a data-img
+//From that data value add tp paramenter for giphy to return images
+//Make Div for buttons
+//Make form input box
+//Make button to add 
+//Link those 
