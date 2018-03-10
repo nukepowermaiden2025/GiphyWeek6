4ecd8492cc25e515a9c07c0b798116ab432ac035
Kourtney Reynolds <Kourtney.Reynolds@gmail.com>
2018-03-09 17:42:36 -0700

    Add giphy base

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,21 @@ var items = ["japan","morracco","egypt","tibet"]
 //Make Div for buttons
 //Make form input box
 //Make button to add more items
+
+
+  $(".gif").on("click", function() {
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    var state = $(this).attr("data-state");
+    if (state == "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "moving");
+    }
+    else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
